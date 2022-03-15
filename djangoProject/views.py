05c75a58327589ab7d614dc43ccdd78e5edf228f
Ahmed Kira <ahmedkira00@gmail.com>
2022-03-15 18:46:09 +0200

    make it better

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index page.")
+
+def about(request):
+    return HttpResponse("Hello, world. You're at the about page.")
+
+
+def courses(request):
+    # render the courses page
+    return render(request, 'courses.html')
